0cbf813cdf3e966ae390fc06110662ea32ecd960
lgxlyh <lgxlyh@gmail.com>
2019-01-01 22:33:50 +0800

    new update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+pydir=/home/lgx/py
+gitdir=/usr/bin
+
+cd ${pydir}
+${gitdir}/python3 ${pydir}/sjget.py >${pydir}/today.tmp
+#mdf=`/usr/bin/git status|grep zhikong|wc -l`
+mdf=`cat today.tmp|wc -l`
+if [ $mdf -gt 0 ];then
+	new=`cat today.tmp`
+	/usr/bin/git commit -a -m $new
+	${gitdir}/git push >/dev/null
+fi
