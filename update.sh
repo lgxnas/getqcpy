fc328bb956b47bdb2ebb93ff3f5e538de2317a98
lgxlyh <lgxlyh@gmail.com>
2019-02-27 00:45:54 +0800

    ft test

--- update.sh
+++ update.sh
@@ -7,9 +7,25 @@ cd ${pydir}
 ${gitdir}/python3 ${pydir}/sjget.py >${pydir}/today.tmp
 #mdf=`/usr/bin/git status|grep zhikong|wc -l`
 mdf=`cat today.tmp|wc -l`
+function ft()
+{
+	title="密码样更新"
+        key=`cat ftsckey`
+        ftdesp="GCP-La by Lgx"
+        for((i=1;i<=$mdf;i++))
+        do
+		id=`sed -n $i"p" today.tmp`
+		ftdesp=$ftdesp"
+	      	"$i". "`grep $id zhikong.txt|awk -F "|" '{print $1"|"$2"|"$4}'`"
+	      	"
+	done
+	curl -s "http://sc.ftqq.com/$key.send?text=$title" -d "&desp=$ftdesp" >>./ft.log
+	echo `date +%F' '%T` >>./ft.log
+}
 if [ $mdf -gt 0 ];then
 	new=`cat today.tmp`
 	/usr/bin/git add .
 	/usr/bin/git commit -m "$new"
 	${gitdir}/git push >/dev/null
+	ft
 fi
